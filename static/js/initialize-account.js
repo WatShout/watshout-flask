caebb0fb1eb176b80fd70856be6116185b06e30e
Ethan Houston <ethan.houston@gmail.com>
2018-07-26 14:28:43 -0700

    New default profile pic

--- static/js/initialize-account.js
+++ static/js/initialize-account.js
@@ -50,7 +50,7 @@ let submitForm = () => {
     }
 
     if (profilePic == null) {
-        errorText += `Please upload a photo <br />`;
+        //errorText += `Please upload a photo <br />`;
     }
 
     if (errorText.length > 0) {
@@ -58,56 +58,131 @@ let submitForm = () => {
         return;
     }
 
-    const typeExtension = profilePic.type.split(`/`)[1].toLowerCase();
+    let typeExtension;
 
-    if (typeExtension !== `png` && typeExtension !== `jpg` && typeExtension !== `jpeg`) {
-        alert(`Wrong image format!`);
-        return;
+    try {
+        typeExtension = profilePic.type.split(`/`)[1].toLowerCase();
+
+        if (typeExtension !== `png` && typeExtension !== `jpg` && typeExtension !== `jpeg`) {
+            alert(`Wrong image format!`);
+            return;
+        }
+    } catch (e) {
+
+        typeExtension = `jpg`;
+
     }
-
-    const name = `profile.` + typeExtension;
-    const metadata = {contentType: profilePic.type};
 
     //ref.child(`users`).child(userID).child(`profile_pic_format`).set(typeExtension);
 
-    ref.child(`users`).child(userID).update({
+    let uploadTask;
+
+    if (profilePic == null){
+        fetch('https://upload.wikimedia.org/wikipedia/commons/7/77/Delete_key1.jpg')
+            .then(res => res.blob()) // Gets the response and returns it as a blob
+            .then(blob => {
+                // Here's where you get access to the blob
+                // And you can use it for whatever you want
+                // Like calling ref().put(blob)
+
+                // Here, I use it to make an image appear on the page
+                typeExtension = `jpg`;
+                console.log(`test`);
+                profilePic = blob;
+
+                let metadata = {contentType: `image/` + typeExtension};
+                const name = `profile.` + typeExtension;
+
+                let thisReference = storageRef.child(`users`).child(userID).child(name);
+
+                ref.child(`users`).child(userID).update({
+                    "profile_pic_format": typeExtension,
+                    "name": userName,
+                    "birthday": String(birthday),
+                    "email": userEmail
+                });
+
+                console.log(profilePic);
+                uploadTask = thisReference.put(profilePic, metadata);
+
+                uploadTask.on('state_changed', function(snapshot){
+                    // Observe state change events such as progress, pause, and resume
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+
+                    document.getElementById(`progress`).innerHTML = progress;
+
+                    switch (snapshot.state) {
+                        case firebase.storage.TaskState.PAUSED: // or 'paused'
+                            console.log('Upload is paused');
+                            console.log(progress);
+                            break;
+                        case firebase.storage.TaskState.RUNNING: // or 'running'
+                            console.log('Upload is running');
+                            console.log(progress);
+                            break;
+                    }
+                }, function(error) {
+                    // Handle unsuccessful uploads
+                }, function() {
+                    // Handle successful uploads on complete
+                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+
+                    location.href = `/`;
+
+
+                });
+
+            });
+    } else {
+
+        let metadata = {contentType: profilePic.type};
+        const name = `profile.` + typeExtension;
+
+        let thisReference = storageRef.child(`users`).child(userID).child(name);
+
+        ref.child(`users`).child(userID).update({
             "profile_pic_format": typeExtension,
             "name": userName,
             "birthday": String(birthday),
             "email": userEmail
         });
 
-    let thisReference = storageRef.child(`users`).child(userID).child(name);
+        console.log(profilePic);
+        uploadTask = thisReference.put(profilePic, metadata);
 
-    let uploadTask = thisReference.put(profilePic, metadata);
+        uploadTask.on('state_changed', function(snapshot){
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
 
-    uploadTask.on('state_changed', function(snapshot){
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
+            document.getElementById(`progress`).innerHTML = progress;
 
-        document.getElementById(`progress`).innerHTML = progress;
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    console.log('Upload is paused');
+                    console.log(progress);
+                    break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                    console.log('Upload is running');
+                    console.log(progress);
+                    break;
+            }
+        }, function(error) {
+            // Handle unsuccessful uploads
+        }, function() {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
 
-        switch (snapshot.state) {
-            case firebase.storage.TaskState.PAUSED: // or 'paused'
-                console.log('Upload is paused');
-                console.log(progress);
-                break;
-            case firebase.storage.TaskState.RUNNING: // or 'running'
-                console.log('Upload is running');
-                console.log(progress);
-                break;
-        }
-    }, function(error) {
-        // Handle unsuccessful uploads
-    }, function() {
-        // Handle successful uploads on complete
-        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-
-        location.href = `/`;
+            location.href = `/`;
 
 
-    });
+        });
+
+
+    }
+
 
 };
